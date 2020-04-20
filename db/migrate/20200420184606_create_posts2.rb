cad08f4c1ecb3781e68544a724f050806f0b4558
Samuel Szabo <samuelszabo7+git@gmail.com>
2020-04-20 20:46:53 +0200

    New Posts table

---
+++ db/migrate/20200420184606_create_posts2.rb
@@ -0,0 +1,10 @@
+class CreatePosts2 < ActiveRecord::Migration[6.0]
+  def change
+    create_table :posts do |t|
+      t.string :text
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
