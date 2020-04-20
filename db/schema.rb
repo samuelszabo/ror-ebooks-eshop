6bce6cb47dd3eef02464ceada24b594fbb30d266
Samuel Szabo <samuelszabo7+git@gmail.com>
2020-04-20 20:44:55 +0200

    Drop post table

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_20_143040) do
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
-    t.index ["user_id"], name: "index_posts_on_user_id"
-  end
+ActiveRecord::Schema.define(version: 2020_04_20_184327) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -33,5 +24,4 @@ ActiveRecord::Schema.define(version: 2020_04_20_143040) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "posts", "users"
 end
