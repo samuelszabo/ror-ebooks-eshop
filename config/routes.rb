bb9006346e340eae9dc213799170a0026ba10685
Samuel Szabo <samuelszabo7+git@gmail.com>
2020-04-20 16:11:00 +0200

    fixes in root, layout and migrated schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'page/home'
-  get 'page/help'
   root 'page#home'
+  get 'page/help'
+
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
