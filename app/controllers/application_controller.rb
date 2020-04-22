0be12dbbd8de1c72307325f4ac0bf27746f75891
Samuel Szabo <samuelszabo7+git@gmail.com>
2020-04-22 20:58:17 +0200

    Require admin to edit products and posts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def require_admin!
+    raise Exception unless current_user&.admin  # this should be some auth exception
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
+  before_action :require_admin!, only: [:new, :edit, :create, :update, :destroy]
 
   # GET /posts
   # GET /posts.json
