f87230e42257cdf8e644839b4c2eff074900be7d
Samuel Szabo <samuelszabo7+git@gmail.com>
2020-04-27 20:56:50 +0200

    fix migration - integer instead of enum

--- db/migrate/20200427185034_create_orders.rb
+++ db/migrate/20200427185034_create_orders.rb
@@ -4,7 +4,7 @@ class CreateOrders < ActiveRecord::Migration[6.0]
       t.belongs_to :user, null: false, foreign_key: true
       t.belongs_to :address, null: false, foreign_key: true
       t.datetime :paid
-      t.enum :state
+      t.integer :state, default: 0
 
       t.timestamps
     end
