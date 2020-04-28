f074dbe72560164599aa31aa5d4e7f1a570d043f
Samuel Szabo <samuelszabo7+git@gmail.com>
2020-04-28 21:22:01 +0200

    Fix OrderItem monetize name

--- db/migrate/20200427185124_create_order_items.rb
+++ db/migrate/20200427185124_create_order_items.rb
@@ -3,7 +3,7 @@ class CreateOrderItems < ActiveRecord::Migration[6.0]
     create_table :order_items do |t|
       t.belongs_to :order, null: false, foreign_key: true
       t.belongs_to :product, null: false, foreign_key: true
-      t.monetize :price_cents
+      t.monetize :price
 
       t.timestamps
     end
