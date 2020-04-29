272c5ddff6f08ba92a21f7b941038da6d15d7970
Samuel Szabo <samuelszabo7+git@gmail.com>
2020-04-29 19:54:17 +0200

    Jquery installed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,14 +7,13 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
 
 import 'materialize-css/dist/js/materialize'
 import 'materialize-css/extras/noUiSlider/nouislider'
 
-// require("jquery")
-
 $(document).ready(function() {
-    $('select').material_select();
+    $('select').formSelect();
 });
 
 // Uncomment to copy all static images under ../images to the output folder and reference
